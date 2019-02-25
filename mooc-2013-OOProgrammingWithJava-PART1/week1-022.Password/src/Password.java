d813a000a1884fa4f177630be9fff756255e5497
Ed Maphis <emaphis85@gmail.com>
2019-02-25 18:11:36 -0500

    Starting loops.

--- mooc-2013-OOProgrammingWithJava-PART1/week1-022.Password/src/Password.java
+++ mooc-2013-OOProgrammingWithJava-PART1/week1-022.Password/src/Password.java
@@ -6,7 +6,23 @@ public class Password {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         String password = "carrot"; // Use carrot as password when running tests.
-
+        String check = "S";
+        String secret = "Carrots are good for the eyes!";
+ 
         // Write your code here
+        while (true) {
+            System.out.print("Type the password: ");
+            check = reader.nextLine();
+
+            if (!check.equals(password)) {
+                System.out.println("Wrong!");
+            } else {
+                System.out.println("Right!");
+                System.out.println("");
+                System.out.print("The secret is: ");
+                System.out.println(secret);
+                break;
+            }
+        }
     }
 }
--- mooc-2013-OOProgrammingWithJava-PART1/week1-023.Temperatures/src/Temperatures.java
+++ mooc-2013-OOProgrammingWithJava-PART1/week1-023.Temperatures/src/Temperatures.java
@@ -6,14 +6,13 @@ public class Temperatures {
     public static void main(String[] args) {
 
         Scanner reader = new Scanner(System.in);
-        // Write your code here. 
 
-        // Graph is used as follows:
-        Graph.addNumber(7);
-        double value = 13.5;
-        Graph.addNumber(value);
-        value = 3;
-        Graph.addNumber(value);
-        // Remove or comment out these lines above before trying to run the tests.
+        // Write your code here. 
+        while (true) {
+            System.out.print("Enter a number ");
+            double number = Double.parseDouble(reader.nextLine());
+            if (number > -31 && number < 40)
+                Graph.addNumber(number);
+        }
     }
 }
