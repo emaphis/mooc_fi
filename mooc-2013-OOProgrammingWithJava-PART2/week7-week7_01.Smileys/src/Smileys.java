82b838e5de7bf1512d5ab0a2f757bd6629bff7e4
Ed Maphis <emaphis85@gmail.com>
2019-03-21 22:58:22 -0400

    Set up Part 2

---
+++ mooc-2013-OOProgrammingWithJava-PART2/week7-week7_01.Smileys/src/Smileys.java
@@ -0,0 +1,11 @@
+
+public class Smileys {
+
+    public static void main(String[] args) {
+        // Test your method at least with the following
+        // printWithSmileys("Method");
+        // printWithSmileys("Beerbottle");
+        // printWithSmileys("Interface");
+    }
+
+}
