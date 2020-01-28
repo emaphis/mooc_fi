d7ef1ea20b52e381bfdfec5bda3266b89842d90c
Ed Maphis <emaphis85@gmail.com>
2020-01-28 00:04:33 -0500

    Finish up.

--- mooc-java-programming-i/part01-Part01_37.GiftTax/src/main/java/GiftTax.java
+++ mooc-java-programming-i/part01-Part01_37.GiftTax/src/main/java/GiftTax.java
@@ -9,14 +9,18 @@ public class GiftTax {
         System.out.println("Value of the gift?");
         int amount = Integer.valueOf(scan.nextLine());
 
-        if (amount <= 5000) {
+        if (amount < 5000) {
             System.out.println("No tax!");
-        } else if (amount <= 25000) {
+        } else if (amount < 25000) {
             System.out.println("Tax: " + (100 + (amount - 5000) * 0.08));
         } else if (amount < 55000) {
             System.out.println("Tax: " + (1700 + (amount - 25000) * 0.10));
-        }       
-    }
-        
+        } else if (amount < 200000) {
+            System.out.println("Tax: " + (4700 + (amount - 55000) * 0.12));
+        } else if (amount < 1000000) {
+            System.out.println("Tax: " + (22100 + (amount - 200000) * 0.15));
+        } else {
+            System.out.println("Tax: " + (142100 + (amount - 1000000) * 0.17));
+        }
     }
 }
