82ed9873d8dc4735e608ee047a5372b141da0401
Ed Maphis <emaphis85@gmail.com>
2020-02-05 16:18:01 -0500

    New exercises.

--- mooc-java-programming-i/part01-Part01_01.Sandbox/src/main/java/Sandbox.java
+++ mooc-java-programming-i/part01-Part01_01.Sandbox/src/main/java/Sandbox.java
@@ -1,25 +1,32 @@
 
+import java.util.Scanner;
+
+
 public class Sandbox {
 
     public static void main(String[] args) {
         // Write your program here
         //System.out.println("Hello, Sandbox!");
-        int number = 0;
 
-    while (true) {
-        number = number + 1;
+        Scanner scanner = new Scanner(System.in);
+        int values = 0;
+        int sum = 0;
 
-        if (number >= 5) {
-            break;
+        while (true) {
+            System.out.println("Provide a value, a negative value ends the program");
+            int value = Integer.valueOf(scanner.nextLine());
+            if (value < 0) {
+                break;
+            }
+
+            values = values + 1;
+            sum = sum + value;
         }
 
-        if (number < 5) {
-            continue;
+        if (sum == 0) {
+            System.out.println("The average of the values could not be calculated.");
+        } else {
+            System.out.println("Average of values: " + (1.0 * sum / values));
         }
-
-        System.out.print(number + " ");
     }
-
-    System.out.print(number + " ");
-}
 }
