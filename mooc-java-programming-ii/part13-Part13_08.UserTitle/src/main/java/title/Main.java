eab47c6e9f15c9bd036672d48f9e340c87ff8b7f
Ed Maphis <emaphis85@gmail.com>
2020-07-15 16:24:19 -0400

    Application parameters in JavaFX

--- mooc-java-programming-ii/part13-Part13_08.UserTitle/src/main/java/title/Main.java
+++ mooc-java-programming-ii/part13-Part13_08.UserTitle/src/main/java/title/Main.java
@@ -1,11 +1,15 @@
 package title;
 
+import java.util.Scanner;
+import javafx.application.Application;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please give me a User's title: ");
+        String title = scanner.nextLine();
+        Application.launch(UserTitle.class, "--title=" + title);
     }
 
 }
