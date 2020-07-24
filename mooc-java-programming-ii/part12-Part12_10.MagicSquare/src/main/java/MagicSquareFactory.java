a2d050bf07e9b741d0ba1c1c1eec0ac097d7d68b
Ed Maphis <emaphis85@gmail.com>
2020-07-24 18:31:24 -0400

    implement siamese method for magic square.

--- mooc-java-programming-ii/part12-Part12_10.MagicSquare/src/main/java/MagicSquareFactory.java
+++ mooc-java-programming-ii/part12-Part12_10.MagicSquare/src/main/java/MagicSquareFactory.java
@@ -2,11 +2,37 @@
 public class MagicSquareFactory {
 
     public MagicSquare createMagicSquare(int size) {
+        if (size % 2 == 0) throw new RuntimeException("size must be odd");
 
         MagicSquare square = new MagicSquare(size);
 
         // implement the creation of a magic square with the Siamese method algorithm here
+        int newNumber = 1;
+        //int lastNumber = size * size;
+
+        int y = 0;
+        int x = (size - 1) / 2;
+        square.placeValue(x, y, newNumber);
+        newNumber++;
+        while (newNumber <= size * size) {
+            int[] newCoord = nextUnit(square, x, y);
+            x = newCoord[0];
+            y = newCoord[1];
+            square.placeValue(x, y, newNumber);
+            newNumber++;
+        }
+
         return square;
     }
 
+    private int[] nextUnit(MagicSquare square, int x, int y) {
+        int nextY = (y - 1 + square.getHeight()) % square.getHeight();
+        int nextX = (x + 1) % square.getWidth();
+        if (square.readValue(nextX, nextY) == 0) {
+            return new int[] {nextX, nextY};
+        } else {
+            y = (y + 1) % square.getWidth();
+            return new int[] {x, y};
+        }
+    }
 }
